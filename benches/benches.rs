523c31bc9563b3801436daf899a8b076e38a2d31
Sander Saares <sander@saares.eu>
2024-12-30 16:56:50 +0200

    Black box

--- benches/benches.rs
+++ benches/benches.rs
@@ -13,58 +13,94 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("naive", |b| {
         b.iter(|| {
-            _ = black_box(naive_solver::solve(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(naive_solver::solve(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_1", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver::solve::<1>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver::solve::<1>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_2", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver::solve::<2>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver::solve::<2>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_4", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver::solve::<4>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver::solve::<4>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_4_u64", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver_u64::solve::<4>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver_u64::solve::<4>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_8", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver::solve::<8>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver::solve::<8>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
 
     c.bench_function("faster_16", |b| {
         b.iter(|| {
-            _ = black_box(faster_solver::solve::<16>(X_A, X_B, X, Y_A, Y_B, Y));
+            _ = black_box(faster_solver::solve::<16>(
+                black_box(X_A),
+                black_box(X_B),
+                black_box(X),
+                black_box(Y_A),
+                black_box(Y_B),
+                black_box(Y),
+            ));
         });
     });
-
-    // The mere existence of these will slow down the above variations (code layout or something?).
-    // c.bench_function("faster_32", |b| {
-    //     b.iter(|| {
-    //         _ = black_box(faster_solver::solve::<32>(x_a, x_b, x, y_a, y_b, y));
-    //     });
-    // });
-
-    // c.bench_function("faster_64", |b| {
-    //     b.iter(|| {
-    //         _ = black_box(faster_solver::solve::<64>(x_a, x_b, x, y_a, y_b, y));
-    //     });
-    // });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/bin/solve_faster_u64.rs
+++ src/bin/solve_faster_u64.rs
@@ -1,17 +1,20 @@
-use std::time::Instant;
+use std::{hint::black_box, time::Instant};
 
 use vectorized_rust::*;
 
 fn main() {
     let start = Instant::now();
 
+    // We use black_box() to prevent the compiler from performing optimizations
+    // that depend on the specific values we provide here. This is because we are
+    // interested in the behavior of this algorithm in the general case.
     let solution = faster_solver_u64::solve::<4>(
-        26,
-        67,
-        12748 + 10000000000000,
-        66,
-        21,
-        12176 + 10000000000000,
+        black_box(26),
+        black_box(67),
+        black_box(12748 + 10000000000000),
+        black_box(66),
+        black_box(21),
+        black_box(12176 + 10000000000000),
     );
     assert_eq!(
         solution,
